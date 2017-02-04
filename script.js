7fc9d28ee3174618a10df272eeeca10e2f5646e2
Grant Glomstead <glomstea@msu.edu>
2017-02-04 14:30:45 -0500

    a short message describing your changes

---
+++ script.js
@@ -0,0 +1,24 @@
+function kickFunction() {
+       var audio = document.getElementById("kick");
+       audio.play();
+                 }
+function snareFunction() {
+       var audio = document.getElementById("snare");
+       audio.play();
+                 }
+function hihatFunction() {
+       var audio = document.getElementById("hihat");
+       audio.play();
+                 }
+function rideFunction() {
+       var audio = document.getElementById("ride");
+       audio.play();
+                 }
+function tomFunction() {
+       var audio = document.getElementById("tom");
+       audio.play();
+                 }
+function clapFunction() {
+       var audio = document.getElementById("clap");
+       audio.play();
+                 }
