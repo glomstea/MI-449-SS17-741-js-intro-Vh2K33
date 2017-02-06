c2bed0ea458cbc50c36d545af75285734379bdb0
Grant Glomstead <glomstea@msu.edu>
2017-02-06 16:19:24 -0500

    Revision

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 function kickFunction() {
-       var audio = document.getElementById("kick");
-       audio.play();
-                 }
+  var audio = document.getElementById("kick");
+  audio.play();
+}
 function snareFunction() {
-       var audio = document.getElementById("snare");
-       audio.play();
-                 }
+  var audio = document.getElementById("snare");
+  audio.play();
+}
 function hihatFunction() {
-       var audio = document.getElementById("hihat");
-       audio.play();
-                 }
+  var audio = document.getElementById("hihat");
+  audio.play();
+}
 function rideFunction() {
-       var audio = document.getElementById("ride");
-       audio.play();
-                 }
+  var audio = document.getElementById("ride");
+  audio.play();
+}
 function tomFunction() {
-       var audio = document.getElementById("tom");
-       audio.play();
-                 }
+  var audio = document.getElementById("tom");
+  audio.play();
+}
 function clapFunction() {
-       var audio = document.getElementById("clap");
-       audio.play();
-                 }
+  var audio = document.getElementById("clap");
+  audio.play();
+}
+document.getElementById("kickbutton").addEventListener("mouseover", kickFunction);
+document.getElementById("kickbutton").addEventListener("click", kickFunction);
+document.getElementById("snarebutton").addEventListener("mouseover", snareFunction);
+document.getElementById("snarebutton").addEventListener("click", snareFunction);
+document.getElementById("hihatbutton").addEventListener("mouseover", hihatFunction);
+document.getElementById("hihatbutton").addEventListener("click", hihatFunction);
+document.getElementById("ridebutton").addEventListener("mouseover", rideFunction);
+document.getElementById("ridebutton").addEventListener("click", rideFunction);
+document.getElementById("tombutton").addEventListener("mouseover", tomFunction);
+document.getElementById("tombutton").addEventListener("click", tomFunction);
+document.getElementById("clapbutton").addEventListener("mouseover", clapFunction);
+document.getElementById("clapbutton").addEventListener("click", clapFunction);
